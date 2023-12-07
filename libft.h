58c6e91483bfd0de738c1a8230e20e15b7d65a92
n-andr <150958639+n-andr@users.noreply.github.com>
2023-12-08 00:40:51 +0100

    added strmapi

--- libft.h
+++ libft.h
@@ -38,3 +38,4 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char    *ft_itoa(int n);
+char    *ft_strmapi(char const *s, char (*f)(unsigned int, char));
