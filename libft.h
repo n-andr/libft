7094e9ee30e5ed2dd842add304ce590deaee5ac6
n-andr <150958639+n-andr@users.noreply.github.com>
2023-12-08 01:31:50 +0100

    upd

--- libft.h
+++ libft.h
@@ -40,3 +40,4 @@ char	*ft_strtrim(char const *s1, char const *set);
 char    *ft_itoa(int n);
 char    *ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar(char c, int fd);
