e1cecc8127ea15e7ac4a0dd1e7f8a0725462f4b6
n-andr <150958639+n-andr@users.noreply.github.com>
2023-12-14 14:38:59 +0100

    Update ft_lstadd_back.c

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -2,6 +2,9 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
+	t_list	*node;
+
+	node = *lst;
 	if (new == NULL || lst == NULL)
 		return ;
 	if (*lst == NULL)
@@ -9,9 +12,33 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 		*lst = new;
 		return ;
 	}
-	while ((*lst)->next)
-	{
-		(*lst) = (*lst)->next;
-	}
-	(*lst)->next = new;
+	while (node->next)
+		node = node->next;
+	node->next = new;
 }
+
+/* #include <stdio.h>
+int main ()
+{
+    t_list *node1 = ft_lstnew("1");
+    t_list *node2 = ft_lstnew("2");
+    t_list *node3 = ft_lstnew("3");
+	t_list	*new = ft_lstnew("0");
+	t_list	*copy = node1;
+
+
+    node1->next = node2;
+    node2->next = node3;
+	while (node1)
+	{
+		printf("%s\n", (char *)node1->content);
+		node1 = node1->next;
+	}
+	ft_lstadd_back(&copy, new);
+	while (copy)
+	{
+		printf("%s\n", (char *)copy->content);
+		copy = copy->next;
+	}
+
+} */
